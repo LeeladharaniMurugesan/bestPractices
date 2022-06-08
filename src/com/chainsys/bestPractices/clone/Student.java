61b17967be1203e8d09947498580b491ee1e6ec7
LeelaMurugesan <leeladharani7901@gmail.com>
2022-06-08 13:29:47 +0530

    first commit

--- src/com/chainsys/bestPractices/clone/Student.java
+++ src/com/chainsys/bestPractices/clone/Student.java
@@ -1,12 +1,11 @@
 package com.chainsys.bestPractices.clone;
 
-public class Student implements Cloneable 
-{
+public class Student implements Cloneable {
 	public String name;
 	public int age;
-	
+
 	@Override
-	protected Object clone() throws CloneNotSupportedException{
+	protected Object clone() throws CloneNotSupportedException {
 		return super.clone();
 	}
 
